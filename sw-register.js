b3b7b688dcf6904061593ac3bf549696a701a12b
zoumiaojiang <zoumiaojiang@gmail.com>
2017-06-02 18:49:04 +0800

    commit weather

--- sw-register.js
+++ sw-register.js
@@ -1,2 +1,2 @@
-webpackJsonp([8],{63:function(e,n){!function(e){if("serviceWorker"in navigator){console.log('支持 service worker'); navigator.serviceWorker.register("/service-worker.js?v=20170602160567",{scope:"/"}).then(function(e){}),navigator.serviceWorker.addEventListener("message",function(e){"updateMessage"===e.data&&n(e)});var n=function(e){location.reload()}}}(window)}},[63]);
-//# sourceMappingURL=sw-register.0c51aab88fce79dc5537.js?v=20170602160539.map
+webpackJsonp([8],{104:function(e,n){!function(e){if("serviceWorker"in navigator){navigator.serviceWorker.register("/service-worker.js?v=20170602184739").then(function(e){}),navigator.serviceWorker.addEventListener("message",function(e){"updateMessage"===e.data&&n(e)});var n=function(e){location.reload()}}}(window)}},[104]);
+//# sourceMappingURL=sw-register.90050d5d9ebf18bdee25.js?v=20170602184739.map
