d5783ac8430fe5935d9ff25511033a871cebfb97
zoumiaojiang <zoumiaojiang@gmail.com>
2017-06-02 16:24:43 +0800

    update vue demo

--- sw-register.js
+++ sw-register.js
@@ -1,2 +1,2 @@
-webpackJsonp([8],{63:function(e,n){!function(e){if("serviceWorker"in navigator){navigator.serviceWorker.register("/service-worker.js?v=20170602160539",{scope:"/"}).then(function(e){}),navigator.serviceWorker.addEventListener("message",function(e){"updateMessage"===e.data&&n(e)});var n=function(e){location.reload()}}}(window)}},[63]);
+webpackJsonp([8],{63:function(e,n){!function(e){if("serviceWorker"in navigator){navigator.serviceWorker.register("/service-worker.js?v=20170602160540",{scope:"/"}).then(function(e){}),navigator.serviceWorker.addEventListener("message",function(e){"updateMessage"===e.data&&n(e)});var n=function(e){location.reload()}}}(window)}},[63]);
 //# sourceMappingURL=sw-register.0c51aab88fce79dc5537.js?v=20170602160539.map
